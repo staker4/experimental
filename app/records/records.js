44fd14ff31ec247f48eeb1e4e0782fa3f126e7ad
staker4 <staker4@gmail.com>
2017-02-08 18:06:07 -0500

    feature: add records ressource
    - Add records ressource  and use it in controller.
    - Display records array in view.

--- app/records/records.js
+++ app/records/records.js
@@ -2,7 +2,29 @@ angular
     .module('app.records')
     .controller('Records', Records);
 
-function Records() {
+function Records($http, $location) {
+
     var vm = this;
-    vm.title = "Records title";
+    vm.title = "Records";
+    vm.records = [];
+
+    activate();
+
+    function activate() {
+        getRecords().then(function (data) {
+            vm.records = data;
+        });
+    }
+
+    function getRecords() {
+        return $http.get('records.json')
+            .then(getDataComplete)
+            .catch(function (message) {
+                $location.url('/');
+            });
+
+        function getDataComplete(data, status, headers, config) {
+            return data.data;
+        }
+    }
 }
